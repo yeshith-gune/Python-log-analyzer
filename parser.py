5a6f3ce9df3bbe2d95ef948270e0b6a5a1003c16
yeshith-gune <yeshithabeysingha@gmail.com>
2026-06-09 06:13:46 +0530

    update

--- parser.py
+++ parser.py
@@ -1,3 +1,10 @@
 import re
 from datetime import datetime
 from models import LogEntry
+# Syslog header: "Jan  3 14:22:01 hostname sshd[1234]:"
+SYSLOG_RE = re.compile(
+    r'^(\w{3}\s+\d{1,2}\s+\d{2}:\d{2}:\d{2})\s+'   # timestamp
+    r'(\S+)\s+'                                        # hostname
+    r'(\w+)(?:\[(\d+)\])?:\s+'                        # service[pid]
+    r'(.+)$'                                           # message body
+)
