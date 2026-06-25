4bb8823e8ba998425e88e55b68f6027a97fb2005
yeshith-gune <yeshithabeysingha@gmail.com>
2026-06-26 04:43:21 +0530

    Update reporter.py

--- reporter.py
+++ reporter.py
@@ -3,3 +3,9 @@ import csv
 import io
 from datetime import datetime
 from models import AttackRecord
+SEVERITY_COLOR = {
+    "low":      "\033[33m",   # yellow
+    "medium":   "\033[33;1m",  # bold yellow
+    "high":     "\033[31m",   # red
+    "critical": "\033[31;1m",  # bold red
+}
